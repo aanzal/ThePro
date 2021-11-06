c9f6201b01b54693cad8ea8c83a0b015d62280fd
Stranger <88586718+aanzal@users.noreply.github.com>
2021-11-06 18:21:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,11 @@
 class script(object):
-    START_TXT = """<b>ആഹാ ഇതാരാ ഇത് {}, സുഖം തന്നെയാ ?,
-ജോലി ഒന്നും ആയില്യോ മോനെ ? ഇങ്ങനെ കളിച്ചു നടന്നാ മതിയോ ??</b>"""
+    START_TXT = """<b>Hello {}.
+I am Just an Advanced Telegram Auto Filter Bot :)
+Bot is Actually Made for <i>@CinemaGround</i>
+
+You need my Help to Create a Bot like me? Click Help / About then!</b>"""
     HELP_TXT = """<b>You know what {}, I won't Help You!</b>"""
-    ABOUT_TXT = """<b>Oh! Nothing Great About me. Thanks for Asking BTW!</b>"""
+    ABOUT_TXT = """<b>Is there no other work in the kitchen?!</b>"""
     SOURCE_TXT = """<b>ആ എടുത്തിട്ട് വരുന്നേ...കുറച്ചു കാത്തിരിക്ക്!</b>"""
     MANUELFILTER_TXT = """<b>അയിന് നീ ഏതാടാ ?</b>"""
     BUTTON_TXT = """<b>അത് വേണോ ഇച്ഛയാ ?!</b>"""
@@ -10,11 +13,11 @@ class script(object):
     CONNECTION_TXT = """<b>നമ്മൾ തമ്മിൽ ഒരു CONNECTION-ഉം ഇല്ല</b>"""
     EXTRAMOD_TXT = """<b>അത്രക്ക് ഒന്നും ഇല്ലപ</b>"""
     ADMIN_TXT = """<b>ഇത് എനിക്ക് ഉള്ളതാണെന്ന് ഇനിക്ക് അറിയാലോ...പിന്നെന്തിനാ നോക്കുന്ന്!</b>"""
-    STATUS_TXT = """➱ <b>TOTAL FILES</b> : <code>{}</code>
-➱ <b>TOTAL USERS</b> : <code>{}</code>
-➱ <b>TOTAL CHATS</b> : <code>{}</code>
-➱ <b>USED STORAGE</b> : <code>{}</code>
-➱ <b>FREE STORAGE</b> : <code>{}</code>"""
+    STATUS_TXT = """➤ <b>𝐓𝐨𝐭𝐚𝐥 𝐅𝐢𝐥𝐞𝐬</b> : <code>{}</code>
+➤ <b>𝐓𝐨𝐭𝐚𝐥 𝐔𝐬𝐞𝐫𝐬</b> : <code>{}</code>
+➤ <b>𝐓𝐨𝐭𝐚𝐥 𝐂𝐡𝐚𝐭𝐬</b> : <code>{}</code>
+➤ <b>𝐔𝐬𝐞𝐝 𝐒𝐭𝐨𝐫𝐚𝐠𝐞</b> : <code>{}</code>
+➤ <b>𝐅𝐫𝐞𝐞 𝐒𝐭𝐨𝐫𝐚𝐠𝐞</b> : <code>{}</code>"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
